aabcf12beaedf5977f6eb3112236cb76ec802ec4
Jesús García Crespo <jesus@sevein.com>
2024-09-18 07:25:55 +0000

    Test clamscan output parse functions

--- tests/unit/test_unit_scanner.py
+++ tests/unit/test_unit_scanner.py
@@ -1,7 +1,12 @@
 from errno import EPIPE
 
+import pytest
+
 from clamav_client.clamd import BufferTooLongError
 from clamav_client.clamd import CommunicationError
+from clamav_client.scanner import ClamscanScanner
+from clamav_client.scanner import Scanner
+from clamav_client.scanner import ScannerInfo
 from clamav_client.scanner import ScanResult
 
 
@@ -49,3 +54,45 @@ def test_scan_result_passed() -> None:
         ScanResult(filename="", state=None, details=None, err=ValueError()).passed
         is False
     )
+
+
+def test_parse_version() -> None:
+    scanner: Scanner = ClamscanScanner({})
+    parse = scanner._parse_version
+
+    parts = parse("ClamAV 0.103.12/27401/Tue Sep 17 10:31:21 2024")
+    assert parts == ScannerInfo(
+        name="ClamAV (clamscan)",
+        version="ClamAV 0.103.12",
+        virus_definitions="27401/Tue Sep 17 10:31:21 2024",
+    )
+
+    parts = parse("ClamAV 0.103.12")
+    assert parts == ScannerInfo(
+        name="ClamAV (clamscan)", version="ClamAV 0.103.12", virus_definitions=None
+    )
+
+    with pytest.raises(ValueError, match="Cannot extract scanner information."):
+        parse("Python 3.12.5")
+
+
+def test_clamscan_scanner_parse_error() -> None:
+    scanner = ClamscanScanner({})
+    parse = scanner._parse_error
+
+    assert parse(None) is None
+    assert parse(1) is None
+    assert parse(b"\xc3\x28") is None  # Invalid UTF-8 byte sequence.
+    assert parse(b"error") == "error"
+
+
+def test_clamscan_scanner_parse_found() -> None:
+    scanner = ClamscanScanner({})
+    parse = scanner._parse_found
+
+    assert parse(None) is None
+    assert parse(1) is None
+    assert parse(b"\xc3\x28") is None  # Invalid UTF-8 byte sequence.
+    assert parse(b"unmatched") is None
+    assert parse(b": FOUND") is None
+    assert parse(b"[...]: file.txt FOUND") == "file.txt"
